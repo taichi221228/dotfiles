33ffe4c0a4fd38f34f8b5ecc5d530ac2ea748efb
taichi221228 <taichi221228@icloud.com>
2024-07-03 12:52:05 +0900

    feat: Add JavaScript file associations to WebStorm
    The commit extends the file associations in the `duti` configuration for WebStorm. It specifically lets WebStorm handle JavaScript source files and two types of unidentified data files identified by their dynamic types.

--- duti/init.zsh
+++ duti/init.zsh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
 
 duti -s com.jetbrains.WebStorm 'public.data' all
+
+duti -s com.jetbrains.WebStorm 'com.netscape.javascript-source' all
+duti -s com.jetbrains.WebStorm 'dyn.ah62d4rv4ge80g4xx' all
+duti -s com.jetbrains.WebStorm 'dyn.ah62d4rv4ge80y652' all
