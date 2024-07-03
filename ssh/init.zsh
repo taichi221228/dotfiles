a7f13cc0f69466ae07634845737b7bdd31430b0f
taichi221228 <taichi221228@icloud.com>
2024-07-03 13:05:10 +0900

    feat: Remove directory deletion in init.zsh scripts
    The deletions for `$HOME/.ssh` and `$HOME/.zshrc` have been removed from the init.zsh scripts. These scripts now solely focus on creating symbolic links to the appropriate directories, reducing potential disruptive actions.

--- ssh/init.zsh
+++ ssh/init.zsh
@@ -1,5 +1,3 @@
 #!/usr/bin/env zsh
 
-rm -r "$HOME/.ssh"
-
 ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Configs/general/ssh/.ssh" "$HOME"
--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,5 +1,3 @@
 #!/usr/bin/env zsh
 
-rm "$HOME/.zshrc"
-
 ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Configs/general/zsh/main.zsh" "$HOME/.zshrc"
