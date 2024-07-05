509038697c89bde7e100643a22a18d020b898646
たいち ひ <taichi221228@icloud.com>
2024-07-05 10:46:44 +0900

    feat: Add new Karabiner configuration files
    This commit introduces the creation of new Karabiner configuration files: bun.lockb, package.json, and src/index.ts. The main purpose of these files is to define key mappings and configurational scripts for the Karabiner environment using typescript and other dependencies.

---
+++ karabiner/src/index.ts
@@ -0,0 +1,32 @@
+import {
+  layer,
+  map,
+  NumberKeyValue,
+  rule,
+  withMapper,
+  writeToProfile,
+} from 'karabiner.ts'
+
+// ! Change '--dry-run' to your Karabiner-Elements Profile name.
+// (--dry-run print the config json into console)
+// + Create a new profile if needed.
+writeToProfile('--dry-run', [
+  // It is not required, but recommended to put symbol alias to layers,
+  // (If you type fast, use simlayer instead, see https://evan-liu.github.io/karabiner.ts/rules/simlayer)
+  // to make it easier to write '←' instead of 'left_arrow'.
+  // Supported alias: https://github.com/evan-liu/karabiner.ts/blob/main/src/utils/key-alias.ts
+  layer('/', 'symbol-mode').manipulators([
+    //     / + [ 1    2    3    4    5 ] =>
+    withMapper(['⌘', '⌥', '⌃', '⇧', '⇪'])((k, i) =>
+      map((i + 1) as NumberKeyValue).toPaste(k),
+    ),
+    withMapper(['←', '→', '↑', '↓', '␣', '⏎', '⇥', '⎋', '⌫', '⌦', '⇪'])((k) =>
+      map(k).toPaste(k),
+    ),
+  ]),
+
+  rule('Key mapping').manipulators([
+    // config key mappings
+    map(1).to(1)
+  ]),
+])
