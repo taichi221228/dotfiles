5b7be2be5822ca18377f3ffc9535549f7ea54a5e
たいち ひ <taichi221228@icloud.com>
2024-07-06 15:09:52 +0900

    feat: Update sym link creation and exclusion rules
    The changes in this commit refactor the symbolic link creation process in the initial zsh profile. The exclusion rule in "prezto/init.zsh" has been updated to ignore the files: "README.md", "zpreztorc" and "zshrc". Also, in "zsh/init.zsh", a symbolic link is now being created for the main zsh configuration file instead of directly sourcing it.

--- prezto/init.zsh
+++ prezto/init.zsh
@@ -4,6 +4,6 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/*(.N); do
-  [[ $rcfile == *README.md ]] && continue
+  [[ $rcfile == *(README.md|zpreztorc|zshrc) ]] && continue
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
--- zsh/init.zsh
+++ zsh/init.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-echo 'source "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Configs/general/zsh/main.zsh"' >>"$HOME/.zshrc"
+ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Configs/general/zsh/main.zsh" "$HOME/.zshrc"
