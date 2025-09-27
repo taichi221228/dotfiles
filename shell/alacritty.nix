2ccbe45afee7eb18fef1c896eacf3854147928a7
taichi221228 <taichi221228@icloud.com>
2025-09-27 21:30:20 +0900

    feat(shell): Add Nushell
    feat(shell): Add Nushell

--- shell/alacritty.nix
+++ shell/alacritty.nix
@@ -1,3 +1,12 @@
+{ vars, ... }:
+let
+  inherit (vars) username;
+in
 {
-  programs.alacritty.enable = true;
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      terminal.shell = "/Users/${username}/.nix-profile/bin/nu";
+    };
+  };
 }
