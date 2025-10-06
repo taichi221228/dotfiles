a56322d6efcd47225c6ac6ca9e58b96cc5570154
taichi221228 <taichi221228@icloud.com>
2025-10-06 11:42:24 +0900

    style: Adjust the order of list items

--- home.nix
+++ home.nix
@@ -8,10 +8,10 @@
   programs.home-manager.enable = true;
   imports = [
     ./core/utils.nix
-    ./appearance/font.nix
     ./app/git.nix
     ./app/jujutsu.nix
     ./app/raycast.nix
+    ./appearance/font.nix
     ./languages/javascript.nix
     ./languages/nix.nix
     ./languages/shell.nix
