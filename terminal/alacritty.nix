67abd1903776af17b3162123814be0941107f4a8
taichi221228 <taichi221228@icloud.com>
2025-10-07 12:31:58 +0900

    feat(terminal): Update Alacritty appearance

--- terminal/alacritty.nix
+++ terminal/alacritty.nix
@@ -6,7 +6,10 @@
         normal.family = "FiraCode Nerd Font";
         size = 16;
       };
-      window.option_as_alt = "Both";
+      window = {
+        option_as_alt = "Both";
+        startup_mode = "SimpleFullscreen";
+      };
     };
   };
 }
