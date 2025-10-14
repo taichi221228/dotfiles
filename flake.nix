7f34977c05362760fda3c1c7c26a6137329eaf99
taichi221228 <taichi221228@icloud.com>
2025-10-14 20:56:33 +0900

    chore(deps): Remove NUR

--- flake.nix
+++ flake.nix
@@ -6,23 +6,17 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur = {
-      url = "github:nix-community/nur";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs =
     {
       nixpkgs,
       home-manager,
-      nur,
       ...
     }:
     {
       homeConfigurations.taichi221228 = home-manager.lib.homeManagerConfiguration {
         pkgs = import nixpkgs {
           system = "aarch64-darwin";
-          overlays = [ nur.overlays.default ];
         };
         modules = [
           ./core/home.nix
