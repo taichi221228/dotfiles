1731fda9d32797f903904f2c67b1f98626a1269c
taichi221228 <taichi221228@icloud.com>
2025-10-21 13:47:47 +0900

    chore(bootstrap): Simplify the Nix installing

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-tmpdir=$(mktemp -d)
-installer="$tmpdir/nix-installer.sh"
-
-cleanup() {
-	rm -rf "$tmpdir"
-}
-
-trap cleanup EXIT
-
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix -o "$installer"
-chmod +x "$installer"
-"$installer" install
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
 
 mkdir -p ~/.config/home-manager
 ln -sfn ~/dotfiles/flake.* ~/.config/home-manager
